42de013ba68235dc31c079f8e2328c6035d01564
mrange <marten_range@hotmail.com>
2014-05-31 13:19:59 +0200

    Added T4 sample

---
+++ MetaProgramming/MetaProgramming/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+using System.ComponentModel;
+
+namespace MetaProgramming
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged (string name)
+        {
+            if (string.IsNullOrEmpty (name)) return;
+
+            var pc = PropertyChanged;
+            if (pc != null)
+            {
+                pc (this, new PropertyChangedEventArgs (name));
+            }
+        }
+    }
+}
