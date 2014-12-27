ed8e8d314483b88165659f640565c00616c193fb
mrange <marten_range@hotmail.com>
2014-12-27 12:46:12 +0100

    Full

--- parsing/mpc/mpc/Program.fs
+++ parsing/mpc/mpc/Program.fs
@@ -4,9 +4,9 @@
 type ParseFailureTree =
     | Empty   
     | Expected      of string
+    | NotExpected   of string
     | Group         of ParseFailureTree list
     | Fork          of ParseFailureTree*ParseFailureTree
-    | EndOfStream
 
 let inline Join (left : ParseFailureTree) (right : ParseFailureTree) = 
     match left, right with
@@ -58,7 +58,7 @@ let parse = ParseBuilder()
 let Atom : Parser<char> =
     fun (str,pos) -> 
         if pos < str.Length then Success str.[pos] Empty str (pos+1)
-        else Failure EndOfStream str pos
+        else Failure (NotExpected "EOS") str pos
 
 let EOS : Parser<unit> = 
     fun (str,pos) -> 
@@ -75,7 +75,17 @@ let Satisfy expected (test : char->bool) : Parser<char> =
             return! failure
     }
 
-let AnyOf (anyOf : string) : Parser<char> = 
+let IsChar (test : char) : Parser<unit> = 
+    let failure = FailWith (Expected (test.ToString()))
+    parse {
+        let! ch = Atom
+        if test = ch then 
+            return ()
+        else 
+            return! failure
+    }
+
+let IsAnyOf (anyOf : string) : Parser<char> = 
     let cs = anyOf.ToCharArray()
     let failure = FailWith (cs |> Array.map (fun ch -> Expected (ch.ToString())) |> List.ofArray |> Group)
     let set = cs |> Set.ofArray
@@ -112,7 +122,6 @@ let Many1 (p : Parser<'T>) : Parser<'T list> =
         let! first  = p
         let! rest   = Many p
         return first::rest
-
     }
 
 let OrElse (left : Parser<'T>) (right : Parser<'T>) : Parser<'T> =
@@ -146,28 +155,66 @@ let SepBy (term : Parser<'T>) (separator : Parser<'S>) (combine : 'T -> 'S -> 'T
     }
 
 type ParseFailure =
-    | WasExpecting  of string
-    | UnexpectedEOS
+    | IsExpecting       of string
+    | IsNotExpecting    of string
 
 let Run (parser : Parser<'T>) (str : string) = 
     let rec collapse acc t = 
         match t with 
         | Empty         -> acc
-        | Expected e    -> (WasExpecting e)::acc
-        | EndOfStream   -> UnexpectedEOS::acc
+        | Expected e    -> (IsExpecting e)::acc
+        | NotExpected e -> (IsNotExpecting e)::acc
         | Group g       -> g |> List.fold collapse acc
         | Fork (l,r)    ->
             let lacc = collapse acc l
             let racc = collapse lacc r
             racc
-    
+
+    let prettify (fs : ParseFailure list) str pos =
+        let sb              = System.Text.StringBuilder(sprintf "Failed at position %d," pos)
+        let isExpecting     = "was expecting"       , " or "
+        let isNotExpecting  = "was not expecting"   , " nor "
+
+        let groupFunction (f : ParseFailure) = 
+            match f with
+            | IsExpecting _     -> isExpecting
+            | IsNotExpecting _  -> isNotExpecting
+
+        let groups = fs |> Seq.groupBy groupFunction |> Array.ofSeq
+        for i in 0..(groups.Length-1) do
+            let (description,last),gfs = groups.[i]
+            let prepend =
+                match i with
+                | 0                         -> " "
+                | _                         -> ", "
+            ignore <| sb.Append prepend
+            ignore <| sb.Append description
+            let gfs = gfs |> Array.ofSeq
+            for i in 0..(gfs.Length-1) do
+                let gf = gfs.[i]
+                let str = 
+                    match gf with
+                    | IsExpecting v     -> v
+                    | IsNotExpecting v  -> v
+                let prepend =
+                    match i with
+                    | 0                         -> " "
+                    | _ when i = gfs.Length-1   -> last
+                    | _                         -> ", "
+                ignore <| sb.Append prepend
+                ignore <| sb.Append str
+
+        ignore <| sb.Append '.'
+        sb.ToString ()
+
     let orv,rf,rstr,rpos = parser (str,0) 
     match orv with
-    | Some rv -> Some rv,[],rstr,rpos
+    | Some rv -> Some rv,"Parse successful",[],rstr,rpos
     | _ -> 
-        let cf = collapse [] rf
-        let f = cf |> Seq.distinct |> List.ofSeq
-        None,f,rstr,rpos
+        let cfs = collapse [] rf
+        let dfs = cfs |> Seq.distinct |> List.ofSeq
+        let msg = prettify dfs rstr rpos
+        None,msg,dfs,rstr,rpos
 
 // ----------------------------------------------------------------------------
 
@@ -196,7 +243,34 @@ type AbstractSyntaxTree =
     | Identifier        of string
     | BinaryOperation   of BinaryOperation*AbstractSyntaxTree*AbstractSyntaxTree
 
+let CharToBinaryOperator ch = 
+    match ch with
+    | '+'   -> Add   
+    | '-'   -> Subtract
+    | '*'   -> Multiply
+    | '/'   -> Divide
+    | _     -> failwith "Unexpected operator: %A" ch
+
+let AddOrSubtract : Parser<BinaryOperation> = 
+    IsAnyOf "+-" >>? CharToBinaryOperator
+
+let MultiplyOrDivide : Parser<BinaryOperation> = 
+    IsAnyOf "*/" >>? CharToBinaryOperator
+
 let Digit : Parser<char> = Satisfy (Expected "digit") System.Char.IsDigit
+
+let SubExpr : Parser<AbstractSyntaxTree> ref = ref (Return (Integer 0))
+
+let MatchedParenthesis : Parser<AbstractSyntaxTree> = 
+    let start   = IsChar '('
+    let stop    = IsChar ')'
+    parse {
+        do! start
+        let! result = !SubExpr
+        do! stop
+
+        return result
+    }
 
 let Integer : Parser<AbstractSyntaxTree> = 
     let pdigits = Many1 Digit
@@ -220,48 +294,90 @@ let Identifier : Parser<AbstractSyntaxTree> =
         return Identifier result
     }
 
-let CharToBinaryOperator ch = 
-    match ch with
-    | '+'   -> Add   
-    | '-'   -> Subtract
-    | '*'   -> Multiply
-    | '/'   -> Divide
-    | _     -> failwith "Unexpected operator: %A" ch
-
-let AddOrSubtract : Parser<BinaryOperation> = 
-    AnyOf "+-" >>? CharToBinaryOperator
-
-let MultiplyOrDivide : Parser<BinaryOperation> = 
-    AnyOf "*/" >>? CharToBinaryOperator
-
 let CombineTerms l op r = BinaryOperation (op,l,r)
 
-let Term : Parser<AbstractSyntaxTree> = Identifier <|> Integer
+let Term : Parser<AbstractSyntaxTree> = Identifier <|> Integer <|> MatchedParenthesis
 
 let MultiplyOrDivideExpr : Parser<AbstractSyntaxTree> = SepBy Term MultiplyOrDivide CombineTerms
 
 let AddOrSubtractExpr : Parser<AbstractSyntaxTree> = SepBy MultiplyOrDivideExpr AddOrSubtract CombineTerms
 
-let Expr = 
+do
+    SubExpr := AddOrSubtractExpr
+
+let FullExpr : Parser<AbstractSyntaxTree> = 
     parse {
         let! expr = AddOrSubtractExpr
         do! EOS
         return expr
     }
 
+let rec Eval (lookup : string -> int) (ast : AbstractSyntaxTree) : int =
+    match ast with
+    | Integer               v   -> v
+    | Identifier            id  -> lookup id
+    | BinaryOperation (bop,l,r) ->
+        let lv = Eval lookup l
+        let rv = Eval lookup r
+        match bop with
+        | Add       -> lv + rv
+        | Subtract  -> lv - rv
+        | Multiply  -> lv * rv
+        | Divide    -> lv / rv
+
+
 // ----------------------------------------------------------------------------
+
+let ColorPrint (cc : System.ConsoleColor) (prelude : string) (str : string) =
+    let saved = System.Console.ForegroundColor
+    System.Console.ForegroundColor <- cc
+    try
+        System.Console.Write prelude
+        System.Console.WriteLine str
+    finally
+        System.Console.ForegroundColor <- saved
+
+let PrintFailure = ColorPrint System.ConsoleColor.Red     "FAILURE: "
+let PrintSuccess = ColorPrint System.ConsoleColor.Green   "SUCCESS: "
 
 [<EntryPoint>]
 let main argv = 
+    let x   = 3
+    let y   = 5
+    let z   = 7
+    let abc = 11
+
+    let lookup str = 
+        match str with
+        | "x"   -> x
+        | "y"   -> y
+        | "z"   -> z
+        | "abc" -> abc
+        | _     -> System.Int32.MaxValue
+
     let tests = 
         [|
-            "abc"
-            "123"
-            "123+456"
-            "x?y+3"
-            "x+y*3"
+            "123+"          , None
+            "abc"           , Some <| abc
+            "123"           , Some <| 123
+            "123+456"       , Some <| 123+456
+            "x?y+3"         , None
+            "x+y*3"         , Some <| x+y*3
+            "x*y+3*(z+x)"   , Some <| x*y+3*(z+x)
         |]
-    for test in tests do 
-        printfn "Parsing: '%s'\nResult: %A" test (Run Expr test)
-
+    for test, expected in tests do 
+        let result, message, failures, _, _ = Run FullExpr test
+        match result, expected with
+        | None, None -> 
+            PrintSuccess <| sprintf "Parsing failed as expected for '%s'\nMessage:%s" test message
+        | Some ast, Some i -> 
+            let actual = Eval lookup ast
+            if actual = i then
+                PrintSuccess <| sprintf "Parsing and evaluation successful for '%s'\nExpected:%i\nActual:%i\nAST:%A" test i actual ast
+            else
+                PrintFailure <| sprintf "Parsing successful but evaluation failed for '%s'\nExpected:%i\nActual:%i\nAST:%A" test i actual ast
+        | None, Some i ->
+            PrintSuccess <| sprintf "Parsing failed for '%s'\nExpected:%i\nMessage:%s" test i message
+        | Some ast, None ->
+            PrintFailure <| sprintf "Parsing successful but expected to fail for '%s'\nAST:%A" test ast
     0
